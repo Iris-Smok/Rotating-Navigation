d561bb2239bd0b7599325def00f75170420e1eda
Iris-Smok <irissmok11@gmail.com>
2022-06-12 00:11:50 +0000

    Add search input

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let open = document.getElementById("open");
 let close = document.getElementById("close");
 let container = document.querySelector(".container");
+let btn = document.querySelector(".btn");
+let search = document.querySelector(".search");
+let input = document.querySelector(".input");
 
 open.addEventListener("click", function () {
   container.classList.add("show-nav");
@@ -8,4 +11,9 @@ open.addEventListener("click", function () {
 
 close.addEventListener("click", function () {
   container.classList.remove("show-nav");
+});
+
+btn.addEventListener("click", function () {
+  search.classList.toggle("active");
+  input.focus();
 });
