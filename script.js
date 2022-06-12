20c8aa6cf7ea9c6335bdfd857c802d93ce5d56de
Iris-Smok <irissmok11@gmail.com>
2022-06-12 00:25:07 +0000

    Add overflow to html to prevent blank scrolling

--- script.js
+++ script.js
@@ -4,13 +4,16 @@ let container = document.querySelector(".container");
 let btn = document.querySelector(".btn");
 let search = document.querySelector(".search");
 let input = document.querySelector(".input");
+let html = document.querySelector("html");
 
 open.addEventListener("click", function () {
   container.classList.add("show-nav");
+  html.style.overflow = "hidden";
 });
 
 close.addEventListener("click", function () {
   container.classList.remove("show-nav");
+  html.style.overflow = null;
 });
 
 btn.addEventListener("click", function () {
